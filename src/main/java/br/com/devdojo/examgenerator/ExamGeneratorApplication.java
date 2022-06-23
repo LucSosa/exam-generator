ac916cb1a6c351cfce41c2619d7ca6be324b5f64
LucSosa <l.sosamachado@gmail.com>
2022-06-23 17:28:36 -0300

    Initial commit

--- src/main/java/br/com/devdojo/examgenerator/ExamGeneratorApplication.java
+++ src/main/java/br/com/devdojo/examgenerator/ExamGeneratorApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExamGeneratorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExamGeneratorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ExamGeneratorApplication.class, args);
+    }
 
 }
